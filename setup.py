128f7b796c6fddc8100b5f907d040d799f832754
sy <somarl@live.com>
2021-06-22 18:24:28 +0800

    fix(pkg): fix license format

--- setup.py
+++ setup.py
@@ -2,11 +2,8 @@ import setuptools
 
 from arakneed import __version__
 
-
-def read_file(p):
-    with open(p, "r") as f:
-        return f.read()
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="arakneed",
@@ -14,15 +11,15 @@ setuptools.setup(
     author="arakneed",
     author_email="arakneed@somarl.com",
     description="A common use targeted concurrent crawler for any directed graph. It's designed to be easy to use.",
-    long_description=read_file("README.md"),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/arakneed/crawler",
     packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=(
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ),
     setup_requires=['wheel'],
-    license=read_file('LICENSE'),
+    license='MIT',
 )
